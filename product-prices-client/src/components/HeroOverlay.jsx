68e9fd140ec6b52b9fd77c1bf8c782e428cd6548
Julian <jsales0204@gmail.com>
2025-07-02 19:15:43 -0400

    first commit boilerplate code for frontend and some backend functionaity

---
+++ product-prices-client/src/components/HeroOverlay.jsx
@@ -0,0 +1,14 @@
+export default function HeroOverlay() {
+    return(
+        <div
+        className="hero min-h-screen bg-[url(/shopping-cart-1923313.png)] bg-top bg-fixed"
+        >
+        <div className="hero-overlay"></div>
+            <div className="hero-content text-center">
+                <div className="max-w-md mb-200">
+
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ product-prices-server/server.js
@@ -0,0 +1,103 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const puppeteer = require("puppeteer");
+//configure cors to accept requests from the frontend server
+const corsOptions = {
+    //vite servers run on port 5173
+    origin : ["http://localhost:5173"],
+};
+
+//initialize app to use cors
+app.use(cors(corsOptions));
+
+app.get("/prices", async (req,res) => {
+    const productsJson = await getProductPrices("iPad mini")
+    res.json(productsJson);
+    //res.send("hi")
+    //console.log(req.query["search"]);
+});
+
+
+//get product prices from product title using puppeteer
+//returns json object of prices
+async function getProductPrices (productTitle) {
+    const offers = [];
+    const allRetailers = {};
+    console.log(`Getting product prices for ${productTitle}...`);
+    
+    const browser = await puppeteer.launch({
+        //toggle on and off for headless (testing)
+        //headless: false,
+        defaultViewport: false,
+    });
+    const page = await browser.newPage();
+    await page.goto(`https://www.bing.com/shop?q=${productTitle}&FORM=SHOPTB`, {waitUntil: 'domcontentloaded'});
+
+    //get all elements on page that show the offer for the product (36 by default)
+    const currentOffers = await page.$$('li.br-item')
+    
+    //go through each offer (list element)
+    for (let i = 0; i < currentOffers.length; i++) {
+
+        //get retailer name
+        const retailerName = await page.evaluate(el => el.querySelector(".br-seller").textContent.toLowerCase(), currentOffers[i]);
+
+        //retailer has not been addeed, so create an object for it
+        if (allRetailers[retailerName] == undefined) {
+            allRetailers[retailerName] = {}
+        } 
+        //ensures no dupicate retailers are added
+        else {
+            continue;
+        }
+
+        //get price of the current offer
+        const offerPrice = await page.evaluate(el => el.querySelector(".resp-one-line.br-max-width").textContent, currentOffers[i]);
+
+        //get the link that displays the link to the retailer's page for this offer
+        const offerLink = await page.evaluate(el => el.querySelector(".l2-crd-clck.sj_spcls").getAttribute("href"), currentOffers[i]);
+        
+        //go to link of offer details on a new page
+        const page2 = await browser.newPage();
+
+
+        /*for some reason the hrefs will sometimes only have part of the link so account for that here */
+
+        //when full link provided
+        if (offerLink.includes("https://www.bing.com")) {
+            await page2.goto(offerLink, {waitUntil: 'domcontentloaded'});
+        }
+        //when part of the link provided
+        else {
+            await page2.goto(`https://www.bing.com/${offerLink}`, {waitUntil: 'domcontentloaded'});
+        }
+
+        /* sometimes an error occurs where the product is not listed anymore, so account for this here
+        and just continue if the element with the retailer link is not found*/
+        try {
+            await page2.waitForSelector('#br-prim-obo > a:nth-child(1)', {visible : true, timeout : 2000})
+        }
+        catch (error) {
+            continue;
+        }
+        //get link to offer listing on retailer's site 
+        const retailerLink = await page2.$eval('#br-prim-obo > a:nth-child(1)', el => el.getAttribute("href"));
+        await page2.close();
+
+        //add info to object
+        offers.push({
+            "retailer" : retailerName,
+            "price": offerPrice,
+            "link": retailerLink
+        });
+    }
+    console.log(`Completed`);
+    await browser.close();    
+    return offers;
+}
+
+//start server on port 8080
+app.listen(8080, () => {
+    console.log("Server started on port 8080");
+})
